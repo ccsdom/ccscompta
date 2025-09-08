ffe6ff85adf9b13dc4a9571b73a99c637de52284
Rabah Mahfouf <app.ccs94@gmail.com>
2025-09-08 05:28:05 +0000

    erreur inconnue s'est produite

--- src/lib/firebase-client.ts
+++ src/lib/firebase-client.ts
@@ -8,7 +8,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   "projectId": "ccs-compta",
   "appId": "1:641289397299:web:160436367ad4dff3e6ef46",
-  "storageBucket": "VOTRE_NOM_DE_BUCKET_ICI.appspot.com",
+  "storageBucket": "ccs-compta.appspot.com",
   "apiKey": "AIzaSyC1Wu-pJ12Ionb9dsjWmaGusuxGmh5LZB4",
   "authDomain": "ccs-compta.firebaseapp.com",
   "messagingSenderId": "641289397299"
