f3704044f32123b7349e4d18ad64ffdd46ce0e33
Rabah Mahfouf <app.ccs94@gmail.com>
2025-09-12 12:33:45 +0000

    Try fixing this error: `Runtime Error: Error: The default Firebase app d

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,15 +2,11 @@ import * as admin from 'firebase-admin';
 
 // Check if the app is already initialized to prevent errors
 if (!admin.apps.length) {
-  try {
     admin.initializeApp({
       // The Project ID can be found in your Firebase project settings.
       // It's a unique identifier for your project.
       // projectId: 'VOTRE_PROJECT_ID', // You might need to set this explicitly in some environments
     });
-  } catch (error) {
-    console.error('Firebase admin initialization error', error);
-  }
 }
 
 const auth = admin.auth();
