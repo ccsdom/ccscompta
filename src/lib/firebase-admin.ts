2b82a8026daf45f074a80f2ced786f63f38c83fb
Rabah Mahfouf <app.ccs94@gmail.com>
2025-09-12 12:34:48 +0000

    Try fixing this error: `Runtime Error: TypeError: Cannot read properties

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,11 +2,7 @@ import * as admin from 'firebase-admin';
 
 // Check if the app is already initialized to prevent errors
 if (!admin.apps.length) {
-    admin.initializeApp({
-      // The Project ID can be found in your Firebase project settings.
-      // It's a unique identifier for your project.
-      // projectId: 'VOTRE_PROJECT_ID', // You might need to set this explicitly in some environments
-    });
+    admin.initializeApp();
 }
 
 const auth = admin.auth();
