f0c03bfe8925b184f0f6e6c64884dbaa94114259
Rabah Mahfouf <app.ccs94@gmail.com>
2025-10-19 19:14:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "ccs-compta",
+  "appId": "1:641289397299:web:160436367ad4dff3e6ef46",
+  "apiKey": "AIzaSyC1Wu-pJ12Ionb9dsjWmaGusuxGmh5LZB4",
+  "authDomain": "ccs-compta.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "641289397299"
+};
